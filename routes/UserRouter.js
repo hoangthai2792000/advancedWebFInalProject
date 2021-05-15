c02a83a55627e6f58df2d75add23780d7812b604
Thai Tran <hoangthai2792000@gmail.com>
2021-05-15 21:09:31 +0700

    make it better

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const formidable = require("formidable");
 const fs = require("fs");
 const { validationResult } = require("express-validator");
